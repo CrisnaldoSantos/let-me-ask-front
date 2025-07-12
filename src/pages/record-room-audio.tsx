5e57d963b162eb438de87b6a27cbeb76ef75e48d
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2025-07-11 23:02:08 -0300

    feat: split the audio in chunks for send to api

--- src/pages/record-room-audio.tsx
+++ src/pages/record-room-audio.tsx
@@ -17,6 +17,7 @@ export default function RecordRoomAudio() {
   const params = useParams<RoomParams>();
   const [isRecording, setIsRecording] = useState(false);
   const recorder = useRef<MediaRecorder | null>(null);
+  const intervalRef = useRef<NodeJS.Timeout>(null);
 
   if (!params.roomId) {
     return <Navigate replace to="/" />;
@@ -34,21 +35,7 @@ export default function RecordRoomAudio() {
     console.log("Upload result:", result);
   }
 
-  async function startRecording() {
-    if (!isRecordingSupported) {
-      alert("O seu navegador não suporta gravação");
-      return;
-    }
-    setIsRecording(true);
-
-    const audio = await navigator.mediaDevices.getUserMedia({
-      audio: {
-        echoCancellation: true,
-        noiseSuppression: true,
-        sampleRate: 44_100,
-      },
-    });
-
+  function createRecorderAudio(audio: MediaStream) {
     recorder.current = new MediaRecorder(audio, {
       mimeType: "audio/webm",
       audioBitsPerSecond: 64_000,
@@ -71,11 +58,38 @@ export default function RecordRoomAudio() {
     recorder.current.start();
   }
 
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert("O seu navegador não suporta gravação");
+      return;
+    }
+    setIsRecording(true);
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    });
+
+    createRecorderAudio(audio);
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop();
+
+      createRecorderAudio(audio);
+    }, 5000);
+  }
+
   function stopRecording() {
     setIsRecording(false);
 
     if (recorder.current && recorder.current.state !== "inactive") {
       recorder.current.stop();
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
     }
   }
 
